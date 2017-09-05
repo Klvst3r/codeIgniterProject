f27f4db9bdbbb688a7cffc0025296459b9ebf46e
klvst3r <klvst3r@gmail.com>
2017-09-04 19:05:55 -0500

    First controller

---
+++ application/controllers/Contactos.php
@@ -0,0 +1,16 @@
+<?php
+//Security Line
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contactos extends CI_Controller {
+
+        public function index()
+        {
+                echo 'Hola Mundo mundial!';
+        }
+}
+
+//In Brobser
+//http://localhost/dev/codeIgniterProject/index.php/contactos
+
+/*End of contactos.php */
